a2d3d1032d29f86342f5ca095416ebb09c142cdf
siongui <siongui@gmail.com>
2018-02-09 06:44:01 +0800

    use environment variable

--- download_test.go
+++ download_test.go
@@ -1,9 +1,13 @@
 package igstorydl
 
 import (
+	"os"
 	"testing"
 )
 
 func ExampleMonitorAndDownload(t *testing.T) {
-	MonitorAndDownload("your user id", "your session id", "your csrftoken")
+	MonitorAndDownload(
+		os.Getenv("IG_DS_USER_ID"),
+		os.Getenv("IG_SESSIONID"),
+		os.Getenv("IG_CSRFTOKEN"))
 }
